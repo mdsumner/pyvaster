9d8547f98b4012e3b2731e98ec3b330d234ee3cd
Michael Sumner <mdsumner@gmail.com>
2018-05-14 20:03:25 +1000

    ignore cache

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stupidest',
+      version='0.1',
+      description='The stupidest package',
+      url='http://github.com/dis-organization/stupidest',
+      author='Michael D. Sumner',
+      author_email='mdsumner@gmail.com',
+      license='GPL-3',
+      packages=['stupidest'],
+      zip_safe=False)
